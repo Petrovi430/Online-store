35d193f29ab7f11af24856d5f8a55cc418107317
Oleksii Hryviuk <petrovi4@i.ua>
2019-03-23 00:29:46 +0200

    Initial commit

---
+++ online-store/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'petrovi4-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'online-store';
+}
